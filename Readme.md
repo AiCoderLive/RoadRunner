d710cc94b8eb0b0e44e56d246e7883f5001fc4ef
Pawel Zelazo <AiCoderLive@hotmail.com>
2024-08-20 22:44:16 +0200

    execute monitoring.py from cmd

--- Readme.md
+++ Readme.md
@@ -1,2 +1,12 @@
+Start the project with the following command:
+```bash
+pip install -r requirements.txt
+```
+Execute monitoring.py with the following command:
+```bash
+python ./src/monitoring.py ./src/results.csv --use_interval
+```
+
+Problems
 - look at the data in the results.csv file. There was problem that miliseconds were not being written in the row with the rest of data, but the row below. After that monitoring.py has got a problem to read it.
 - ![img.png](img.png)
--- src/monitoring.py
+++ src/monitoring.py
@@ -3,62 +3,87 @@ from dash import dcc, html
 from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.express as px
+import argparse
 
 
 class Monitoring:
-    def __init__(self, csv_file):
+    def __init__(self, csv_file, use_interval=True):
         self.csv_file = csv_file
+        self.use_interval = use_interval
         self.app = dash.Dash(__name__)
         self.df = None
         self.setup_app()
 
     def load_data(self):
-        self.df = pd.read_csv(self.csv_file)
+        try:
+            self.df = pd.read_csv(self.csv_file)
+            self.process_data()
+        except FileNotFoundError:
+            print(f"Error: File {self.csv_file} not found.")
+            exit(1)
 
-        # Przetwarzanie kolumn czasu
+    def process_data(self):
         self.df['StartTime'] = pd.to_datetime(self.df['StartTime'], format='%H:%M:%S:%f')
         self.df['EndTime'] = pd.to_datetime(self.df['EndTime'], format='%H:%M:%S:%f')
 
     def setup_app(self):
         self.load_data()
+        self.app.layout = self.create_layout()
+        if self.use_interval:
+            self.setup_callbacks()
 
-        self.app.layout = html.Div(children=[
+    def create_layout(self):
+        layout_children = [
             html.H1(children='Monitoring aktywnych użytkowników'),
-
-            dcc.Graph(id='active-users-graph'),
-            dcc.Graph(id='response-time-graph'),  # Nowy komponent dla wykresu ResponseTime
-
-            dcc.Interval(
-                id='interval-component',
-                interval=1 * 1000,  # in milliseconds
-                n_intervals=0
+            dcc.Graph(id='active-users-graph', figure=self.create_active_users_graph()),
+            dcc.Graph(id='response-time-graph', figure=self.create_response_time_graph())
+        ]
+        if self.use_interval:
+            layout_children.append(
+                dcc.Interval(
+                    id='interval-component',
+                    interval=1 * 1000,  # in milliseconds
+                    n_intervals=0
+                )
             )
-        ])
+        return html.Div(children=layout_children)
 
+    def setup_callbacks(self):
         @self.app.callback(
             Output('active-users-graph', 'figure'),
             Input('interval-component', 'n_intervals')
         )
         def update_active_users_graph(n):
-            fig = px.line(self.df, x="StartTime", y="VusersNumber", title="Aktywni użytkownicy w czasie")
-            fig.update_xaxes(tickformat="%H:%M:%S")
-            fig.update_xaxes(dtick=1000, tick0=self.df['StartTime'].min())
-            return fig
+            return self.create_active_users_graph()
 
         @self.app.callback(
             Output('response-time-graph', 'figure'),
             Input('interval-component', 'n_intervals')
         )
         def update_response_time_graph(n):
-            fig = px.line(self.df, x="EndTime", y="ResponseTime[ms]", title="Czas odpowiedzi w czasie")
-            fig.update_xaxes(tickformat="%H:%M:%S")
-            fig.update_xaxes(dtick=1000, tick0=self.df['EndTime'].min())
-            return fig
+            return self.create_response_time_graph()
+
+    def create_active_users_graph(self):
+        fig = px.line(self.df, x="StartTime", y="VusersNumber", title="Aktywni użytkownicy w czasie")
+        fig.update_xaxes(tickformat="%H:%M:%S")
+        fig.update_xaxes(dtick=1000, tick0=self.df['StartTime'].min())
+        return fig
+
+    def create_response_time_graph(self):
+        fig = px.line(self.df, x="EndTime", y="ResponseTime[ms]", title="Czas odpowiedzi w czasie")
+        fig.update_xaxes(tickformat="%H:%M:%S")
+        fig.update_xaxes(dtick=1000, tick0=self.df['EndTime'].min())
+        return fig
 
     def run(self):
         self.app.run_server(debug=True)
 
 
 if __name__ == '__main__':
-    monitoring = Monitoring('results.csv')
+    parser = argparse.ArgumentParser(description='Run the Monitoring app.')
+    parser.add_argument('csv_file', type=str, help='Path to the CSV file.')
+    parser.add_argument('--use_interval', action='store_true', help='Enable interval refresh.')
+    args = parser.parse_args()
+
+    monitoring = Monitoring(args.csv_file, use_interval=args.use_interval)
     monitoring.run()
