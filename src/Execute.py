029f24cc53bce59dce0f12da68b4613709ce662d
Pawel Zelazo <aicoderlive@hotmail.com>
2024-12-12 22:34:24 +0100

    Change name from Run to Scenario

--- src/Execute.py
+++ src/Execute.py
@@ -1,24 +1,24 @@
 import csv
 
-from src.Run import Run
+from src.Scenario import Scenario
 from src.Request import Request
 
 # Initialize and prepare CSV file for results
-with open(Run.results_file, mode='w', newline='') as file:
+with open(Scenario.results_file, mode='w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(["VusersNumber", "URL", "StartTime", "EndTime", "ResponseTime[ms]"])
 
 # Definicja scenariuszy
 # scenario1 = (Scenario("First URL").set_url("http://httpbin.org/get").set_method("GET").set_headers(""))
-scenario1 = (Request("First URL").set_url("https://www.wp.pl/").set_method("GET").set_headers(""))
+request1 = (Request("First URL").set_url("https://www.wp.pl/").set_method("GET").set_headers(""))
 # .save("X-Amzn-Trace-Id"))
-# scenario2 = Scenario("Second URL").set_url("http://httpbin.org/post").set_method("POST").set_headers("").set_body(
+# request2 = Scenario("Second URL").set_url("http://httpbin.org/post").set_method("POST").set_headers("").set_body(
 #     "X-Amzn-Trace-Id: {X-Amzn-Trace-Id}")
-# scenario3 = Scenario("Third URL").set_url("http://httpbin.org/put").set_method("PUT").set_headers("")
+# request3 = Scenario("Third URL").set_url("http://httpbin.org/put").set_method("PUT").set_headers("")
 
-Run.requests.append(scenario1)
-# Run.scenarios.append(scenario2)
-# Run.scenarios.append(scenario3)
+Scenario.requests.append(request1)
+# Run.scenarios.append(request2)
+# Run.scenarios.append(request3)
 
 # Execute tests
 # Run.speed(1, 20)  # 1 user for 10 seconds
@@ -26,7 +26,7 @@ Run.requests.append(scenario1)
 # Run.speed(5, 20)  # 10 users for 20 seconds
 # Run.speed(20, 20)  # 5 users for 20 seconds
 
-Run.once() # Execute all scenarios sequentially, once each
+Scenario.once() # Execute all scenarios sequentially, once each
 
 # ARIMA Model Implementation
 
--- src/Scenario.py
+++ src/Scenario.py
@@ -3,7 +3,7 @@ import datetime
 import threading
 import time
 
-class Run:
+class Scenario:
     results_file = 'results.csv'
     requests = []
     threads = []
@@ -11,13 +11,13 @@ class Run:
 
     @staticmethod
     def speed(users, duration):
-        for request in Run.requests:
+        for request in Scenario.requests:
             for _ in range(users):
-                thread = threading.Thread(target=Run.run_scenario, args=(request, users, duration))
-                Run.threads.append(thread)
+                thread = threading.Thread(target=Scenario.run_scenario, args=(request, users, duration))
+                Scenario.threads.append(thread)
                 thread.start()
 
-        for thread in Run.threads:
+        for thread in Scenario.threads:
             thread.join()
 
     @staticmethod
@@ -25,7 +25,7 @@ class Run:
         start_time = time.time()
         while time.time() - start_time < duration:
             response = scenario.print_response()
-            Run.log_result(vusers, scenario.url, response)
+            Scenario.log_result(vusers, scenario.url, response)
 
     @staticmethod
     def log_result(vusers, url, response):
@@ -33,8 +33,8 @@ class Run:
         try:
             start_time = end_time - response.elapsed
             response_time = round(response.elapsed.total_seconds() * 1000, 3)  # Round the response time
-            with Run.lock:  # Lock the block of code to ensure thread safety
-                with open(Run.results_file, mode='a', newline='') as file:
+            with Scenario.lock:  # Lock the block of code to ensure thread safety
+                with open(Scenario.results_file, mode='a', newline='') as file:
                     writer = csv.writer(file)
                     writer.writerow(
                         [vusers, url, start_time.strftime('%H:%M:%S:%f')[:-3],
@@ -46,7 +46,7 @@ class Run:
     # Execute all scenarios sequentially, once each
     @staticmethod
     def once():
-        for request in Run.requests:
+        for request in Scenario.requests:
             response = request.print_response()
             # Logging with a vuser number of 1 as it's a single execution
-            Run.log_result(1, request.url, response)
+            Scenario.log_result(1, request.url, response)
